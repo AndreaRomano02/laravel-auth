19522c058a023e0535dff32b85806b51c12d0efd
AndreaRomano02 <andy.romano2002@gmail.com>
2023-08-31 18:04:41 +0200

    Mino edit On edti Form Projects

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -10,7 +10,7 @@
         <div class="mb-3 col-6">
             <label for="title" class="form-label">Titolo</label>
             <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
-                value="{{ $project->title }}" required>
+                value="{{ old('title', $project->title) }}" required>
             <div class="invalid-feedback">
                 {{ $errors->first('title') }}
             </div>
@@ -18,7 +18,7 @@
         <div class="mb-3 col-6">
             <label for="url" class="form-label">URL</label>
             <input type="url" class="form-control @error('url') is-invalid @enderror" name="url" id="url"
-                value="{{ $project->url }}" required>
+                value="{{ old('url', $project->url) }}" required>
             <div class="invalid-feedback">
                 {{ $errors->first('url') }}
             </div>
@@ -26,14 +26,14 @@
         <div class="mb-3 col-12">
             <label for="img" class="form-label">Immagine</label>
             <input type="url" class="form-control @error('img') is-invalid @enderror" name="img" id="img"
-                value="{{ $project->img }}">
+                value="{{ old('img', $project->img) }}">
             <div class="invalid-feedback">
                 {{ $errors->first('img') }}
             </div>
         </div>
         <div class="col-12 mb-3">
             <label for="description" class="form-label @error('description') is-invalid @enderror">Descrizione</label>
-            <textarea class="form-control" name="description" id="description" rows="5">{{ $project->description }}</textarea>
+            <textarea class="form-control" name="description" id="description" rows="5">{{ old('description', $project->description) }}</textarea>
             <div class="invalid-feedback">
                 {{ $errors->first('description') }}
             </div>
