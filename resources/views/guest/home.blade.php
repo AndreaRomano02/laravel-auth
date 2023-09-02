5e3ede88193341885027a3ae3d71e1a90f130c9e
AndreaRomano02 <andy.romano2002@gmail.com>
2023-09-02 17:08:09 +0200

    Add image effects opacity and last projects

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -7,11 +7,25 @@
                 <div class="col-8 py-4 left">
                     <h1>Hello World</h1>
                     <p>I'm Andrea Romano and I am Junior Full Stack Web Developer.</p>
-                    <p></p>
+
+                    <div class="last-projects">
+                        @forelse ($projects as $project)
+                            <div class="project-title">
+                                <a href="#">{{ $project->title }}</a>
+                            </div>
+                        @empty
+                            <h2>Non ci sono progetti</h2>
+                        @endforelse
+                    </div>
                 </div>
                 <div class="col-4 right">
                 </div>
             </div>
         </div>
+        <img class="image-home" src="{{ Vite::asset('resources/img/andrea.jpg') }}" alt="Andrea">
     </div>
+@endsection
+
+@section('scripts')
+    @vite('resources/js/guest/home.js')
 @endsection
